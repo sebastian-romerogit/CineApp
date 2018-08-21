1007131d9a67432505f35a8b2d3768a6d9f9174d
sebax <sebax@HP>
2018-08-21 00:07:58 -0500

    Modificaciones métodos cineApp

--- src/cineapp/CineApp.java
+++ src/cineapp/CineApp.java
@@ -1,6 +1,6 @@
-
-
 package cineapp;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -8,9 +8,55 @@ package cineapp;
  */
 public class CineApp {
 
-   
+    public static String menu = "\n 1. vender silleteria"
+            + " \n 2. Mostrar estado de la sala"
+            + "\n 3. Mostrar venta de la funciones"
+            + "\n 4. Nueva función";
+
+    public static int sillas[] = new int[100];
+
     public static void main(String[] args) {
-        // Code aplication
+        int respuesta = operar();
+        System.out.println(respuesta);
+        
+        while (respuesta == 0) {
+            int opcion = funcion();
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("ok1");
+                    break;
+
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
     }
+
+    public static int funcion() {
+
+        int opcion = datoEntero("Seleccione una opción " + menu);
+
+        return opcion;
+    }
+
+    public static String dato(String solicitud) {
+        String dato = JOptionPane.showInputDialog(solicitud);
+        return dato;
+    }
+
+    public static int datoEntero(String solicitud) {
+        int dato = Integer.parseInt(JOptionPane.showInputDialog(solicitud));
+        return dato;
+    }
+    
+    public static int operar(){
+    int dato = JOptionPane.showConfirmDialog(null, "¿Desea realizar una nueva operación?",null, JOptionPane.YES_NO_OPTION);
+    return dato;}
 
 }
