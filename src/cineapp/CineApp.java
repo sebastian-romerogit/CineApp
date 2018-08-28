e5957d2609f5e61f7bf54d72dd783496ec18ebf2
sebax <sebax@HP>
2018-08-28 12:28:34 -0500

    Modificaciones métodos del main

--- src/cineapp/CineApp.java
+++ src/cineapp/CineApp.java
@@ -13,19 +13,24 @@ public class CineApp {
             + "\n 3. Mostrar venta de la funciones"
             + "\n 4. Nueva función"
             + "\n 5. Mostrar el mayor"
-            +"\n 6. Mostrar el menor";
+            + "\n 6. Mostrar el menor"
+            + "\n 7. Mostrar orden de usuarios";
 
     // public static int sillas[] = new int[100];
+    
     public static int sillas[] = new int[100];
     public static int sillasOrden[] = new int[100];
+    
+    public static int numeros[] = {};
 
-    public static int menor = 0;
-    public static int mayor = 0;
-    public static int temporal = 0;
+    public static int menor = 0; //Variable para almacenar el menor del cine
+    public static int mayor = 0; //Variable para almacenar el mayor del cine
+    public static int posicion = 0; //Variable global para almacenar posición del menor actual
 
     public static void main(String[] args) {
         int respuesta = 0;
 
+        //Ejecución de operaciones
         while (respuesta == 0) {
             int opcion = funcion();
             switch (opcion) {
@@ -48,9 +53,13 @@ public class CineApp {
                     mayor();
                     respuesta = operar();
                     break;
-                    
-                case 6: 
+
+                case 6:
                     menor();
+                    respuesta = operar();
+                    break;
+                case 7:
+                    ordenar();
                     respuesta = operar();
                     break;
             }
@@ -58,6 +67,7 @@ public class CineApp {
 
     }
 
+    //Mpetodo para elegir operación a realizar
     public static int funcion() {
 
         int opcion = datoEntero("Seleccione una opción " + menu);
@@ -65,6 +75,7 @@ public class CineApp {
         return opcion;
     }
 
+    //Método para reservar una silla
     public static void silleteria() {
         String estado = estadoSala();
         int silla = datoEntero("¿Qué silla desea \n" + estado);
@@ -72,24 +83,38 @@ public class CineApp {
         sillas[silla] = datoEntero("Ingrese su cedula");
 
     }
-    
-    public static void ordenar(){
+
+    //Método para ordenar cédulas en segundo arreglo
+    public static void ordenar() {
+        //int temp;
+
+        for (int i = 0; i < sillas.length; i++) {
+            menor = 0;
+            posicion = 0;
+            menor(); //Método de encontrar el menor en arreglo principal
+            sillasOrden[i] = menor; //Asignación de valor
+            sillas[posicion] = 0;
+        }
+
+//        for (int i = 0; i < sillasOrden.length; i++) {
+//            System.out.println("--" + sillasOrden[i]);
+//        }
         //Variable para llevar conteo de datos ingresados
         //Reemplazar valor por 0
-    
+
     }
 
     public static void mayor() {
-       int temp, posicion=0;
-       
+        int temp;
+
         for (int i = 0; i < sillas.length; i++) {
             if (sillas[i] != 0) {
                 mayor = sillas[i];
-                posicion =i;
+                posicion = i;
                 break;
             }
         }
-               
+
         for (int i = posicion; i < sillas.length; i++) {
             if (sillas[i] != 0) {
                 temp = sillas[i];
@@ -103,18 +128,18 @@ public class CineApp {
         JOptionPane.showMessageDialog(null, "El usuario mayor está en la posición: " + posicion + "\n Su cédula es: " + mayor);
 
     }
-    
-      public static void menor() {
-       int temp, posicion=0;
-       
+
+    public static void menor() {
+        int temp;
+
         for (int i = 0; i < sillas.length; i++) {
             if (sillas[i] != 0) {
                 menor = sillas[i];
-                posicion =i;
+                posicion = i;
                 break;
             }
         }
-               
+
         for (int i = posicion; i < sillas.length; i++) {
             if (sillas[i] != 0) {
                 temp = sillas[i];
@@ -125,7 +150,10 @@ public class CineApp {
             }
 
         }
-        JOptionPane.showMessageDialog(null, "El usuario menor está en la posición: " + posicion + "\n Su cédula es: " + menor);
+        if (menor != 0) {
+
+            JOptionPane.showMessageDialog(null, "El usuario menor está en la posición: " + posicion + "\n Su cédula es: " + menor);
+        }
 
     }
 
@@ -143,16 +171,19 @@ public class CineApp {
         return estado;
     }
 
+    //Método para solicitar un dato de tipo String
     public static String dato(String solicitud) {
         String dato = JOptionPane.showInputDialog(solicitud);
         return dato;
     }
 
+    //método para solicitar dato de tipo entero
     public static int datoEntero(String solicitud) {
         int dato = Integer.parseInt(JOptionPane.showInputDialog(solicitud));
         return dato;
     }
 
+    //Método para preguntar si seguir en la app
     public static int operar() {
         int dato = JOptionPane.showConfirmDialog(null, "¿Desea realizar una nueva operación?", null, JOptionPane.YES_NO_OPTION);
         return dato;
